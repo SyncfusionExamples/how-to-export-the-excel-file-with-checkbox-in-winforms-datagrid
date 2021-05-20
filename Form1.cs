1481306d2e583509ce12e9da63fe5352941538b4
vijayarasan <58214164+vijayarasan@users.noreply.github.com>
2021-05-20 10:55:04 +0530

    Sample Added
    Sample Added

---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using Syncfusion.Data;
+using Syncfusion.WinForms.DataGrid;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using Syncfusion.WinForms.DataGridConverter;
+using Syncfusion.XlsIO;
+
+namespace SfDataGridDemo
+{
+    /// <summary>
+    /// Summary description for Form1.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            sfDataGrid.AutoGenerateColumns = false;
+            sfDataGrid.DataSource = new ViewModel().Orders;
+            sfDataGrid.LiveDataUpdateMode = LiveDataUpdateMode.AllowDataShaping;
+            sfDataGrid.AllowEditing = true;
+
+            GridNumericColumn gridTextColumn1 = new GridNumericColumn() { MappingName = "OrderID", HeaderText = "Order ID" };
+            GridTextColumn gridTextColumn2 = new GridTextColumn() { MappingName = "CustomerID", HeaderText = "Customer ID" };
+            GridTextColumn gridTextColumn3 = new GridTextColumn() { MappingName = "CustomerName", HeaderText = "Customer Name" };
+            GridTextColumn gridTextColumn4 = new GridTextColumn() { MappingName = "Country", HeaderText = "Country" };
+            GridTextColumn gridTextColumn5 = new GridTextColumn() { MappingName = "ShipCity", HeaderText = "Ship City" };
+            GridCheckBoxColumn checkBoxColumn = new GridCheckBoxColumn() { MappingName = "IsShipped", HeaderText = "Is Shipped" };
+
+            sfDataGrid.Columns.Add(gridTextColumn1);
+            sfDataGrid.Columns.Add(gridTextColumn2);
+            sfDataGrid.Columns.Add(gridTextColumn3);
+            sfDataGrid.Columns.Add(gridTextColumn4);
+            sfDataGrid.Columns.Add(gridTextColumn5);
+            sfDataGrid.Columns.Add(checkBoxColumn);
+
+            btnExportExcel.Click += BtnExportExcel_Click;
+        }
+
+        ExcelExportingOptions GridExcelExportingOptions = new ExcelExportingOptions();
+
+        private ExcelExportingOptions ExcelExportingOptions1()
+        {
+            GridExcelExportingOptions.ExportAllPages = true;
+            GridExcelExportingOptions.AllowOutlining = true;
+            GridExcelExportingOptions.CellExporting += Options_CellExporting1;
+            return GridExcelExportingOptions;
+        }
+
+        private void BtnExportExcel_Click(object sender, EventArgs e)
+        {
+            var excelEngine = sfDataGrid.ExportToExcel(sfDataGrid.View, ExcelExportingOptions1());
+            var workBook = excelEngine.Excel.Workbooks[0];
+
+            SaveFileDialog saveFilterDialog = new SaveFileDialog
+            {
+                FilterIndex = 2,
+                Filter = "Excel 97 to 2003 Files(*.xls)|*.xls|Excel 2007 to 2010 Files(*.xlsx)|*.xlsx|Excel 2013 File(*.xlsx)|*.xlsx",
+                FileName = "Sample1"
+            };
+
+            if (saveFilterDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (Stream stream = saveFilterDialog.OpenFile())
+                {
+                    if (saveFilterDialog.FilterIndex == 1)
+                        workBook.Version = ExcelVersion.Excel97to2003;
+                    else if (saveFilterDialog.FilterIndex == 2)
+                        workBook.Version = ExcelVersion.Excel2016;
+                    else
+                        workBook.Version = ExcelVersion.Excel2013;
+                    workBook.SaveAs(stream);
+                }
+
+                //Message box confirmation to view the created workbook.
+                if (MessageBox.Show(this.sfDataGrid, "Do you want to view the workbook?", "Workbook has been created",
+                                    MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                {
+
+                    //Launching the Excel file using the default Application.[MS Excel Or Free ExcelViewer]
+                    System.Diagnostics.Process.Start(saveFilterDialog.FileName);
+                }
+            }
+        }
+
+        private void Options_CellExporting1(object sender, Syncfusion.WinForms.DataGridConverter.Events.DataGridCellExcelExportingEventArgs e)
+        {
+            // Based on the column mapping name and the cell type, we can change the cell values while exporting to excel.
+            if (e.CellType == ExportCellType.RecordCell && e.ColumnName == "IsShipped")
+            {
+                //add the checkbox into excel shhet
+                var checkbox = e.Range.Worksheet.CheckBoxes.AddCheckBox(e.Range.Row, e.Range.Column, 20, 20);
+
+                //set the checked or unchecked state based on cell value
+                if (e.CellValue.ToString().ToLower() == "true")
+                    checkbox.CheckState = ExcelCheckState.Checked;
+                else if (e.CellValue.ToString().ToLower() == "false")
+                    checkbox.CheckState = ExcelCheckState.Unchecked;
+
+                //Created check box with cell link
+                checkbox.LinkedCell = e.Range.Worksheet[e.Range.AddressLocal];
+
+                e.Handled = true;
+            }
+        }
+    }
+
+    public class OrderInfo : INotifyPropertyChanged
+    {
+        decimal? orderID;
+        string customerId;
+        string country;
+        string customerName;
+        string shippingCity;
+        bool isShipped;
+
+        public OrderInfo()
+        {
+
+        }
+
+        public decimal? OrderID
+        {
+            get { return orderID; }
+            set { orderID = value; this.OnPropertyChanged("OrderID"); }
+        }
+
+        public string CustomerID
+        {
+            get { return customerId; }
+            set { customerId = value; this.OnPropertyChanged("CustomerID"); }
+        }
+
+        public string CustomerName
+        {
+            get { return customerName; }
+            set { customerName = value; this.OnPropertyChanged("CustomerName"); }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; this.OnPropertyChanged("Country"); }
+        }
+
+        public string ShipCity
+        {
+            get { return shippingCity; }
+            set { shippingCity = value; this.OnPropertyChanged("ShipCity"); }
+        }
+
+        public bool IsShipped
+        {
+            get { return isShipped; }
+            set { isShipped = value; this.OnPropertyChanged("IsShipped"); }
+        }
+
+
+        public OrderInfo(decimal? orderId, string customerName, string country, string customerId, string shipCity, bool isShipped)
+        {
+            this.OrderID = orderId;
+            this.CustomerName = customerName;
+            this.Country = country;
+            this.CustomerID = customerId;
+            this.ShipCity = shipCity;
+            this.IsShipped = isShipped;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class ViewModel
+    {
+        private ObservableCollection<OrderInfo> orders;
+        public ObservableCollection<OrderInfo> Orders
+        {
+            get { return orders; }
+            set { orders = value; }
+        }
+
+        public ViewModel()
+        {
+            orders = new ObservableCollection<OrderInfo>();
+            orders.Add(new OrderInfo(1001, "Thomas Hardy", "Germany", "ALFKI", "Berlin", true));
+            orders.Add(new OrderInfo(1002, "Laurence Lebihan", "Mexico", "ANATR", "Mexico", false));
+            orders.Add(new OrderInfo(1003, "Antonio Moreno", "Mexico", "ANTON", "Mexico", true));
+            orders.Add(new OrderInfo(1004, "Thomas Hardy", "UK", "AROUT", "London", true));
+            orders.Add(new OrderInfo(1005, "Christina Berglund", "Sweden", "BERGS", "Lula", false));
+        }
+    }
+}
